5636ad55d607f779057a04a6b840dffa67395f66
정민우 <meanwo1017@gmail.com>
2021-09-13 12:53:06 +0900

    9/13 6ban class commit 1

--- djangoProject/settings/base.py
+++ djangoProject/settings/base.py
@@ -16,19 +16,10 @@ from pathlib import Path
 from django.contrib import messages
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4b=@1k(5p1izd^wz6i9#92iygc_+_b94zo#s)wq9bbmn6mgu5a'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
@@ -87,12 +78,6 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
